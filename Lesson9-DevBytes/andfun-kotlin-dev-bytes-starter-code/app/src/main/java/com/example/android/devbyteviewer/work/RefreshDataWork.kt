abac4eeffb33b7ecc6d937f7b34d4799f5ac0557
Lukas Stransky <lukas.stransky.at@gmail.com>
2019-12-28 14:48:01 +0100

    9.19 - Schedule Background Work - Lukas Stransky

--- Lesson9-DevBytes/andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
+++ Lesson9-DevBytes/andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
@@ -26,10 +26,17 @@ import retrofit2.HttpException
 
 class RefreshDataWorker(appContext: Context, params: WorkerParameters):
         CoroutineWorker(appContext, params) {
+
+    companion object {
+        const val WORK_NAME = "RefreshDataWorker"
+    }
+
+    /**
+     * A coroutine-friendly method to do your work.
+     */
     override suspend fun doWork(): Payload {
         val database = getDatabase(applicationContext)
         val repository = VideosRepository(database)
-
         return try {
             repository.refreshVideos()
             Payload(Result.SUCCESS)
